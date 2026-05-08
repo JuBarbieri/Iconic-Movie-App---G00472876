b24cea664afa14e1e203369e9595428a25d78775
JuBarbieri <julianbarbieriposca@gmail.com>
2026-05-08 17:14:57 +0000

    Add app routes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+
+//all the routes for the app
+export const routes: Routes =[
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        loadComponent: () => improt('./home/home.page').then(m => m.HomePage),
+    },
+    {
+        path: 'movie-details',
+        loadComponent: () => import('./movie-details/movie-details.page').then(m => m.MovieDetailsPage),
+    },
+    {
+        path: 'details'
+        loadComponent: () => import('./details/details.page').then(m => m.DetailsPage),
+    },
+    {
+        path: 'favourites'
+        loadComponent: () => import('./favourites/favourites.page').then(m => m.FavouritesPage),
+    },
+];
