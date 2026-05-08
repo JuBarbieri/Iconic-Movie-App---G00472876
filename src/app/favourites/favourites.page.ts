986aff7accc21d14319629dda741168a63951f60
JuBarbieri <julianbarbieriposca@gmail.com>
2026-05-08 23:10:28 +0000

    Add favourites page

---
+++ src/app/favourites/favourites.page.ts
@@ -0,0 +1,55 @@
+import { Componet } from '@angular/core';
+import { CommonModule } from '@angular/commmon';
+import { Router } from '@angular/router';
+import {
+    IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, ViewWillEnter
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { home } from 'ionicons/icons';
+import { FavouritesService } from '../services/favourites.service';
+import { MovieService } from '../services/movie.service';
+import { DataService } from '../services/data.service';
+
+@Component({
+    selector: 'app-favourites',
+    templateUrl: 'favourites.page.html',
+    styleUrl: ['favourites.page.scss'],
+    standalone: true,
+    imports: [
+        CommonModule,
+            IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon
+    ],
+})
+export class FavouritesPage implements ViewWillEnter {
+
+    favourites: any = [];
+
+    constructor(
+        private favouritesService: FavouritesService,
+        private movieService: MovieService,
+        private dataService: DataService,
+        private router: Router
+    ){
+        addIcons({ home });
+    }
+
+    //reload favourites every time the page is opened
+    async ionViewWillEnter() {
+        this.favourites = await this.favouritesService.getFavourites();
+        console.log(this.favourites);
+    }
+
+    //store selected movie and go to movie details page
+    openMovieDetails(movie: any) {
+        this.dataService.selectedMovie = movie;
+        this.router.navigate(['/movie-details']);
+    }
+
+    goHome(){
+        this.router.navigate(['/home']);
+    }
+
+    goImageUrl(path: string) {
+        return this.movieService.getImageUrl(path);
+    }
+}
